b08df2c0f0cef3b888beaf2ebb0853912373c71d
MehdiZed24 <mehdiziani1989@gmail.com>
2021-07-08 10:07:28 +0200

    Ajout route GET/PATCH/DELETE exercice

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+const { request } = require("http");
 const { ObjectId } = require("mongodb");
 
 // Connexion à la BDD
@@ -29,7 +30,7 @@ fastify.get("/heroes", async () => {
 });
 
 // heroes/69 GET Obtiens le héros ayant l'id 69.
-//heroes GET - Obitiens la lste des héros
+//heroes GET - Obtiens la lste des héros
 fastify.get("/heroes/:heroesId", async (request, reply) => {
   // Documentation  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
   const heroesId = request.params.heroesId;
@@ -45,10 +46,14 @@ fastify.get("/heroes/bio/:heroesId", async (request, reply) => {
   const collection = fastify.mongo.db.collection("heroes");
   const { heroesId } = request.params;
   const result = await collection.findOne({
-    _id: new ObjectId(heroesId)
-  })
+    _id: new ObjectId(heroesId),
+  });
 
-  const {name, biography, powerstats:{intelligence,speed}} = result
+  const {
+    name,
+    biography,
+    powerstats: { intelligence, speed },
+  } = result;
   return `${name} connu sous le nom de ${biography["full-name"]}. Je suis née à ${biography["place-of-birth"]}. J'ai ${intelligence} en intelligence, et ${speed} en vitesse.`;
 });
 
@@ -60,28 +65,25 @@ fastify.post("/heroes", async (request, reply) => {
   // reply.send(null)
 });
 
-
-fastify.delete('/heroes/:heroesId', async (request, reply)=>{
-  const collection = fastify.mongo.db.collection("heroes")
-  const {heroesId} = request.params 
+fastify.delete("/heroes/:heroesId", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("heroes");
+  const { heroesId } = request.params;
   const result = await collection.findOneAndDelete({
-    _id: new ObjectId(heroesId)
-    })
-    return result
-})
+    _id: new ObjectId(heroesId),
+  });
+  return result;
+});
 
-
-fastify.patch('/heroes/:id', async (request,reply)=>{
-  const collection = fastify.mongo.db.collection('heroes')
-  const {id}= request.params
+fastify.patch("/heroes/:id", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("heroes");
+  const { id } = request.params;
   const result = await collection.findOneAndUpdate(
-    {_id: new ObjectId(id) }, 
-    {$set :request.body },
-    {returnDocument:'after'}
-)
-  return result
-})
-
+    { _id: new ObjectId(id) },
+    { $set: request.body },
+    { returnDocument: "after" }
+  );
+  return result;
+});
 
 fastify.get("/me", function () {
   return {
@@ -90,6 +92,72 @@ fastify.get("/me", function () {
     job: "developpeur Junior",
   };
 });
+
+// Je souhaite:
+// Une route qui me permette de créer un nouvel utilisateur (user) dans une collection users
+// 		- email
+// 		- password
+// 		- role (user/admin)
+// Une route qui me permette de récupérer tout les utilisateurs
+// Une route qui me permette de récupérer un utilisateur par son id
+// Une route qui me permette de mettre à jour un utilisateur par son id
+// Une route qui me permette de supprimer un utilisateur par son id
+
+/*fastify.get("/users", function () {
+  return {
+    email: email,
+    password: password,
+    role: "user"
+  };
+});
+*/
+
+fastify.post("/users", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("users");
+  const result = await collection.insertOne(request.body);
+  return result.ops[0];
+});
+
+fastify.get("/users", async () => {
+  const collection = fastify.mongo.db.collection("users");
+  const result = await collection.find({}).toArray();
+  return result;
+});
+
+fastify.get("/users/:usersId", async (request, reply) => {
+  const {usersId} = request.params
+  const collection = fastify.mongo.db.collection("users");
+  const result = await collection.findOne({
+    _id: new ObjectId(usersId),
+  });
+  return result;
+});
+
+fastify.patch("/users/:id", async (request, reply) => {
+  const collection = fastify.mongo.db.collection("users");
+  const {id} = request.params;
+  const result = await collection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: request.body },
+    { returnDocument: "after" }
+  );
+  reply.code(200).send(result)
+});
+
+fastify.delete("/users/:id",async(request,reply)=>{
+  
+})
+
+/*fastify.get("/heroes/:heroesId", async (request, reply) => {
+  // Documentation  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+  const heroesId = request.params.heroesId;
+  const collection = fastify.mongo.db.collection("heroes");
+  const result = await collection.findOne({
+    _id: new ObjectId(heroesId),
+  });
+  return result;
+});
+*/
 
 // Run the server!
 const start = async () => {
